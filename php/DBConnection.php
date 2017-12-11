659d5e5f091f0dfc806bb3e096de34f58fb8ccae
RC Pelisco <rcpelisco@gmail.com>
2017-12-11 12:05:48 +0800

    Updated DB Config

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -1,5 +1,8 @@
 <?php
-$mysql = mysqli_connect("localhost", "root", "", "utang_list");
+date_default_timezone_set("Asia/Manila");
+$mysql = mysqli_connect("localhost", "id978681_rcpelisco", "09991537566", "id978681_utang_list");
+$mysql->query("SET SESSION time_zone = '+8:00'");
 if($mysql->error)
 	die("Database error: " . $mysql->error);
+
 ?>
